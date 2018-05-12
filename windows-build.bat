d0649ef06ef679435c5d6a3e40eb6c66ba93a73c
Jay Lee <jay@home.jaylee.us>
2018-05-12 14:05:02 -0400

    Wix 3.11

--- windows-build.bat
+++ windows-build.bat
@@ -16,6 +16,6 @@ xcopy LICENSE chrome-bisect\
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip chrome-bisect-%1-windows.zip chrome-bisect\ -xr!.svn
 
 set CBVERSION=%1
-"%ProgramFiles(x86)%\WiX Toolset v3.10\bin\candle.exe" -arch x64 chrome-bisect.wxs
-"%ProgramFiles(x86)%\WiX Toolset v3.10\bin\light.exe" -ext "%ProgramFiles(x86)%\WiX Toolset v3.10\bin\WixUIExtension.dll" chrome-bisect.wixobj -o chrome-bisect-%1-windows.msi
+"%ProgramFiles(x86)%\WiX Toolset v3.11\bin\candle.exe" -arch x64 chrome-bisect.wxs
+"%ProgramFiles(x86)%\WiX Toolset v3.11\bin\light.exe" -ext "%ProgramFiles(x86)%\WiX Toolset v3.11\bin\WixUIExtension.dll" chrome-bisect.wixobj -o chrome-bisect-%1-windows.msi
 del /q /f chrome-bisect-%1-windows.wixpdb
