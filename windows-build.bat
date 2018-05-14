4c1d171caf92615020abf599770f3f094af93e8f
Jay Lee <jayhlee@google.com>
2018-05-14 13:31:51 -0400

    Add Windows shortcut, prep for 32-bit build

--- windows-build.bat
+++ windows-build.bat
@@ -11,6 +11,6 @@ xcopy LICENSE chrome-bisect\
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip chrome-bisect-%1-windows.zip chrome-bisect\ -xr!.svn
 
 set CBVERSION=%1
-"%ProgramFiles(x86)%\WiX Toolset v3.11\bin\candle.exe" -arch x64 chrome-bisect.wxs
+"%ProgramFiles(x86)%\WiX Toolset v3.11\bin\candle.exe" -arch x86 chrome-bisect.wxs
 "%ProgramFiles(x86)%\WiX Toolset v3.11\bin\light.exe" -ext "%ProgramFiles(x86)%\WiX Toolset v3.11\bin\WixUIExtension.dll" chrome-bisect.wixobj -o chrome-bisect-%1-windows.msi
 del /q /f chrome-bisect-%1-windows.wixpdb
