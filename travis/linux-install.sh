d68ad5e2ee76efc3e7b9d211a90da4818cc32737
Jay Lee <jay0lee@gmail.com>
2020-10-30 14:56:09 -0400

    change name in install.sh scripts

--- travis/linux-install.sh
+++ travis/linux-install.sh
@@ -1,21 +1,16 @@
-if [[ "$TRAVIS_JOB_NAME" == *"Testing" ]]; then
-  export me="$python -m break19"
-  export mypath=$(readlink -e .)
-else
-  export mypath="dist/break19"
+  export mypath="dist/chrome-bisect"
   rm -rf $mypath
   mkdir -p $mypath
   export mypath=$(readlink -e $mypath)
-  $python -OO -m PyInstaller --clean --noupx --strip -F --distpath $mypath break19.spec
-  export me="${mypath}/break19"
+  $python -OO -m PyInstaller --clean --noupx --strip -F --distpath $mypath chrome-bisect.spec
+  export me="${mypath}/chrome-bisect"
   export MYVERSION=`$me --version`
   cp LICENSE $mypath
-  MY_ARCHIVE=break19-$MYVERSION-$MYOS-$PLATFORM.tar.xz
+  MY_ARCHIVE=chrome-bisect-$MYVERSION-$MYOS-$PLATFORM.tar.xz
   # tar will cd to dist and tar
-  tar -C dist/ --create --file $MY_ARCHIVE --xz break19
+  tar -C dist/ --create --file $MY_ARCHIVE --xz chrome-bisect
   echo "PyInstaller info:"
   du -h $me
   time $me version
   echo "My packages:"
-  ls -l break19-*.tar.xz
-fi
+  ls -l chrome-bisect-*.tar.xz
--- travis/windows-install.sh
+++ travis/windows-install.sh
@@ -1,21 +1,21 @@
 echo "compiling with pyinstaller..."
-export mypath="dist/break19"
+export mypath="dist/chrome-bisect"
 rm -rf $mypath
 mkdir -p $mypath
 export mypath=$(readlink -e $mypath)
-pyinstaller --clean --noupx -F --distpath $mypath break19.spec
-export me="${mypath}/break19"
+pyinstaller --clean --noupx -F --distpath $mypath chrome-bisect.spec
+export me="${mypath}/chrome-bisect"
 echo "running compiled..."
 $me --version
 export MYVERSION=`$me --version`
 cp LICENSE $mypath
-MY_ARCHIVE=break19-$MYVERSION-$MYOS-$PLATFORM.zip
+MY_ARCHIVE=chrome-bisect-$MYVERSION-$MYOS-$PLATFORM.zip
 /c/Program\ Files/7-Zip/7z.exe a -tzip $MY_ARCHIVE $mypath -xr!.svn
 
 echo "Running WIX candle $WIX_BITS..."
-/c/Program\ Files\ \(x86\)/WiX\ Toolset\ v3.11/bin/candle.exe -arch $WIX_BITS break19.wxs
+/c/Program\ Files\ \(x86\)/WiX\ Toolset\ v3.11/bin/candle.exe -arch $WIX_BITS chrome-bisect.wxs
 echo "Done with WIX candle..."
 echo "Running WIX light..."
-/c/Program\ Files\ \(x86\)/WiX\ Toolset\ v3.11/bin/light.exe -ext /c/Program\ Files\ \(x86\)/WiX\ Toolset\ v3.11/bin/WixUIExtension.dll break19.wixobj -o break19-$MYVERSION-$MYOS-$PLATFORM.msi || true;
+/c/Program\ Files\ \(x86\)/WiX\ Toolset\ v3.11/bin/light.exe -ext /c/Program\ Files\ \(x86\)/WiX\ Toolset\ v3.11/bin/WixUIExtension.dll chrome-bisect.wixobj -o chrome-bisect-$MYVERSION-$MYOS-$PLATFORM.msi || true;
 echo "Done with WIX light..."
 rm *.wixpdb
