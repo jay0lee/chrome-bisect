4b8ecc2886f7c4b8658513fa5584eb19473709b0
Jay Lee <jay0lee@gmail.com>
2021-06-21 09:15:49 -0400

    chrome_bisect.py cleanup

--- chrome_bisect.py
+++ chrome_bisect.py
@@ -13,73 +13,89 @@ override_milestones = {
         }
 
 def get_relative_chrome_versions(minus=0):
-    url = 'https://versionhistory.googleapis.com/v1/chrome/platforms/all/channels/stable/versions/all/releases?fields=releases%2Fversion%2CnextPageToken'
+    ''' returns current Chrome stable milestone number minus value of minus'''
+    url = 'https://versionhistory.googleapis.com' \
+          '/v1/chrome/platforms/all/channels/stable/versions/all/releases' \
+          '?fields=releases%2Fversion%2CnextPageToken'
     resp = requests.get(url)
     versions = resp.json().get('releases')
     milestones = []
-    for v in versions:
-        ver = v.get('version')
+    for version in versions:
+        ver = version.get('version')
         if ver:
-            m = int(ver.split('.')[0])
-            if m not in milestones:
-                milestones.append(m)
+            milestone = int(ver.split('.')[0])
+            if milestone not in milestones:
+                milestones.append(milestone)
     milestones.sort(reverse=True)
     return milestones[minus]
 
+def detect_archive():
+    '''returns an appropriate value for archive based on current OS and architecture.'''
+    myos = system()
+    sixty_four = sys.maxsize > 2**32
+    mymachine = machine()
+    archive = None
+    if myos == 'Windows':
+        if sixty_four:
+            archive = 'win64'
+        else:
+            archive = 'win'
+    elif myos == 'Linux':
+        if sixty_four:
+            archive = 'linux64'
+        else:
+            archive = 'linux'
+    elif myos == 'Darwin':
+        if mymachine == 'arm64':
+            archive = 'mac-arm'
+        elif mymachine == 'x86_64':
+            archive = 'mac64'
+    if not archive:
+        print('Could not auto-detect value for --archive. ' \
+              f'OS: {myos}, 64-bit: {sixty_four}, machine: {mymachine}')
+        sys.exit(1)
+    return archive
+
+
 def add_default_args(args):
+    '''sets appropriate default arguments for bisect-builds.py'''
     # return --help quick
     if '--help' in args or '-h' in args:
         return args
-    
+
     # always add --verify-range and --use-local-cache
     for def_arg in ['--verify-range', '--use-local-cache']:
         if def_arg not in args:
             args.append(def_arg)
 
+    # if --archive is not set detect an appropriate value to set
     archive_set = False
     for arg in list(args):
         arg = arg.split('=')[0]
-        if arg == '-a' or arg == '--archive':
+        if arg in ['-a', '--archive']:
             archive_set = True
     if not archive_set:
-        myos = system()
-        sixty_four = sys.maxsize > 2**32
-        mymachine = machine()
-        archive = None
-        if myos == 'Windows':
-            if sixty_four:
-                archive = 'win64'
-            else:
-                archive = 'win'
-        elif myos == 'Linux':
-            if sixty_four:
-                archive = 'linux64'
-            else:
-                archive = 'linux'
-        elif myos == 'Darwin':
-            if mymachine == 'arm64':
-                archive = 'mac-arm'
-            elif mymachine == 'x86_64':
-                archive = 'mac64'
-        if not archive:
-            print(f'Could not auto-detect value for --archive. OS: {myos}, 64-bit: {sixty_four}, machine: {mymachine}')
-            sys.exit(1)
+        archive = detect_archive()
         args.extend(['--archive', archive])
+
+    # if --good is not set set to Chrome stable milestone - 6
     good_set = False
     for arg in list(args):
         arg = arg.split('=')[0]
-        if arg == '-g' or arg == '--good':
+        if arg in ['-g', '--good']:
             good_set = True
     if not good_set:
         good = get_relative_chrome_versions(6)
         good_milestone = f'M{good}'
         args.extend(['--good', good_milestone])
+
     # fix milestones with bad build results
     for i in range(0, len(args) - 1):
         args[i] = override_milestones.get(args[i].upper(), args[i])
     return args
 
 def main():
+    '''main screen turn on'''
     if '--' in sys.argv:
         split_at = sys.argv.index('--')
         bisect_args = sys.argv[:split_at]
