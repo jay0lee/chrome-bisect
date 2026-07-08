eed7d837d1b47296935837d5264318606b21a0cf
Jay Lee <jay0lee@gmail.com>
2026-07-08 16:34:05 -0400

    numerous fixes and improvements

--- chrome_bisect.py
+++ chrome_bisect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 ''' wrapper for bisect-builds.py which simplifies option setting '''
-from codecs import decode
+import codecs
 from platform import system, machine
 import os
 import ssl
@@ -29,15 +29,17 @@ def unsecure_urllib_request_opener():
             handler._context = insecure_context
 
 
-def get_relative_chrome_versions(minus=0):
-    ''' returns current Chrome stable milestone number minus value of minus'''
-    global verify
+def get_relative_chrome_versions(minus=0, verify=True):
+    '''Returns current Chrome stable milestone number minus value of minus.'''
     url = 'https://versionhistory.googleapis.com' \
           '/v1/chrome/platforms/all/channels/stable/versions/all/releases' \
           '?fields=releases%2Fversion%2CnextPageToken'
-    print(f'verify is {verify}.')
     resp = requests.get(url, verify=verify)
+    resp.raise_for_status()
     versions = resp.json().get('releases')
+    if not versions:
+        print('ERROR: No release data returned from versionhistory API.')
+        sys.exit(1)
     milestones = []
     for version in versions:
         ver = version.get('version')
@@ -75,8 +77,13 @@ def detect_archive():
     return archive
 
 
-def add_default_args(args):
-    '''sets appropriate default arguments for bisect-builds.py'''
+def _arg_present(args, *flags):
+    '''Check if any of the given flags are present in args.'''
+    return any(arg.split('=')[0] in flags for arg in args)
+
+
+def add_default_args(args, verify=True):
+    '''Sets appropriate default arguments for bisect-builds.py.'''
     
     detected_archive = detect_archive()
 
@@ -96,35 +103,25 @@ def add_default_args(args):
             args.append(def_arg)
 
     # if --archive is not set detect an appropriate value to set
-    archive_set = False
-    for arg in list(args):
-        arg = arg.split('=')[0]
-        if arg in ['-a', '--archive']:
-            archive_set = True
-    if not archive_set:
+    if not _arg_present(args, '-a', '--archive'):
         args.extend(['--archive', detected_archive])
 
     # if --good is not set set to Chrome stable milestone - 6
-    good_set = False
-    for arg in list(args):
-        arg = arg.split('=')[0]
-        if arg in ['-g', '--good']:
-            good_set = True
-    if not good_set:
-        good = get_relative_chrome_versions(6)
+    if not _arg_present(args, '-g', '--good'):
+        good = get_relative_chrome_versions(6, verify=verify)
         good_milestone = f'M{good}'
         args.extend(['--good', good_milestone])
 
     return args
 
 def add_default_chrome_args(args):
-    '''sets appropriate arguments that bisect-builds.py will pass to Chromium binaries'''
+    '''Sets appropriate arguments that bisect-builds.py will pass to Chromium binaries.'''
 
     args.append('--enable-chrome-browser-cloud-management')
     return args
     
 def main():
-    '''main screen turn on'''
+    '''Parse arguments, configure environment, and run bisect-builds.py.'''
     if '--' in sys.argv:
         split_at = sys.argv.index('--')
         bisect_args = sys.argv[:split_at]
@@ -132,7 +129,6 @@ def main():
     else:
         bisect_args = sys.argv
         chrome_args = ['--']
-    global verify
     verify = True
     if '--do-not-verify-tls' in bisect_args:
         unsecure_urllib_request_opener()
@@ -149,12 +145,12 @@ def main():
     
     # secret, not really secret
     if not os.environ.get('GOOGLE_API_KEY'):
-        os.environ['GOOGLE_API_KEY'] = decode('NVmnFlObSBm7wRPR5dUolxDHcnyXg_lUV4YKYTp', 'rot13')
+        os.environ['GOOGLE_API_KEY'] = codecs.decode('NVmnFlObSBm7wRPR5dUolxDHcnyXg_lUV4YKYTp', 'rot13')
     if not os.environ.get('GOOGLE_DEFAULT_CLIENT_SECRET'):
-        os.environ['GOOGLE_DEFAULT_CLIENT_SECRET'] = decode('TBPFCK-LDdWORqtJDArsuKlwsQBXee5bY8W', 'rot13')
+        os.environ['GOOGLE_DEFAULT_CLIENT_SECRET'] = codecs.decode('TBPFCK-LDdWORqtJDArsuKlwsQBXee5bY8W', 'rot13')
     if not os.environ.get('GOOGLE_DEFAULT_CLIENT_ID'):
-        os.environ['GOOGLE_DEFAULT_CLIENT_ID'] = decode('933175750481-eo2epca4c5a4wnyueudoefv1nryusa8p.nccf.tbbtyrhfrepbagrag.pbz', 'rot13')
-    bisect_args = add_default_args(bisect_args)
+        os.environ['GOOGLE_DEFAULT_CLIENT_ID'] = codecs.decode('933175750481-eo2epca4c5a4wnyueudoefv1nryusa8p.nccf.tbbtyrhfrepbagrag.pbz', 'rot13')
+    bisect_args = add_default_args(bisect_args, verify=verify)
     chrome_args = add_default_chrome_args(chrome_args)
     sys.argv = bisect_args + chrome_args
     print(f'running bisect-builds.py with options: {" ".join(sys.argv[1:])}')
